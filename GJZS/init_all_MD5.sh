00dec18d8240a422ff281e6770531e6b7ebcd248
ligus <75608937+ligusx@users.noreply.github.com>
2021-10-04 07:54:43 +0800

    Update init_all_MD5.sh

--- GJZS/init_all_MD5.sh
+++ GJZS/init_all_MD5.sh
@@ -4,6 +4,6 @@
     Util_Functions_MD5=27d8bea2e557810265c71e596ff519ca
     Cloud_ID=Cloud_Page.zip
     Cloud_MD5=0975f95a6b10fd1c5d7cd4dc9c94e5a8
-    Applet_ID=Misc/Applet.zip
+    Applet_ID=Applet.zip
     Applet_MD5=4b72333cf5ca2edff20743f58de71cbb
     Cloud_Version=2021060418
